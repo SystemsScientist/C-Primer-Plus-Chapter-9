383f3b8fe9a042847bdd5ddf4839a168a8ec5cae
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-29 07:04:40 -0500

    Adding Listing-9-7

---
+++ Listing-9-7/factor.c
@@ -0,0 +1,53 @@
+// factor.c -- uses loops and recursion to calculate factorials
+
+#include <stdio.h>
+
+// function prototypes
+long fact(int);
+long rfact(int);
+
+int main(void) {
+	
+	int num;
+
+	printf("This program calculates factorials.\n");
+	printf("Enter a value in the range 0 - 12 (q to quit): \n");
+	while (scanf("%d", &num) == 1) {
+		if (num < 0)
+			printf("No negative numbers, please.\n");
+		else if (num > 12)
+			printf("Keep input under 13.\n");
+		else {
+			printf("loop: %d factorial = %ld\n",
+				num, fact(num));
+			printf("recursion: %d factorial = %ld\n",
+				num, rfact(num));
+		}
+		printf("Enter a value in the range of 0 - 12 (q to quit): \n");
+	}
+	printf("Bye.\n");
+
+	return 0;
+}
+
+long fact(int n) {
+	
+	long ans;
+
+	for (ans = 1; n > 1; n--)
+		ans *= n;
+
+	return ans;
+}
+
+long rfact(int n) {
+
+	long ans;
+
+	if (n > 0)
+		ans = n * rfact(n - 1);
+	else
+		ans = 1;
+
+	return ans;
+}
