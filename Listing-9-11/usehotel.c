710addba40be323cd38166243f12fc4a97af3d73
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-03 08:34:06 -0500

    Adding Listing-9-11

---
+++ Listing-9-11/usehotel.c
@@ -0,0 +1,40 @@
+// usehotel.c -- room rate program
+// compile with hotel.c by using the following
+// command-line scripts:
+// $ gcc hotel.c usehotel.c -o main
+// $ ./main
+
+#include <stdio.h>
+#include "hotel.h" // defines constants, declares functions
+
+int main(void) {
+	
+	int nights;
+	double hotel_rate;
+	int code;
+
+	while ((code = menu()) != QUIT) {
+		switch (code) {
+			case 1: 
+				hotel_rate = HOTEL1;
+				break;
+			case 2: 
+				hotel_rate - HOTEL2;
+				break;
+			case 3:
+				hotel_rate = HOTEL3;
+				break;
+			case 4:
+				hotel_rate = HOTEL4;
+				break;
+			default:
+				hotel_rate = 0.0;
+				break;
+		}
+		nights = getnights();
+		showprice(hotel_rate, nights);
+	}
+	printf("Thank you and goodbye.\n");
+
+	return 0;
+}
