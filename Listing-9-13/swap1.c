86f04256284f939db92a4bc3a3f1c384bbe4d7b7
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-04 05:36:06 -0500

    Adding Listing-9-13

---
+++ Listing-9-13/swap1.c
@@ -0,0 +1,25 @@
+// swap1.c -- first attempt at a swapping function
+
+#include <stdio.h>
+
+void interchange(int, int); // function prototype
+
+int main(void) {
+
+	int x = 5, y = 10;
+
+	printf("Originally x = %d and y = %d.\n", x, y);
+	interchange(x, y); // function call
+	printf("Now x = %d and y = %d.\n", x, y);
+
+	return 0;
+}
+
+void interchange(int u, int v) { // define function
+
+	int temp;
+
+	temp = u;
+	u = v;
+	v = temp;
+}
